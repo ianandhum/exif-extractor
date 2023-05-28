aa923641ff899865215b5157efff6a658479a8eb
Anandhu Manoj <38727945+ianandhum@users.noreply.github.com>
2023-05-28 17:18:49 +0000

    fix: typo - test labels

--- export/html_export_test.go
+++ export/html_export_test.go
@@ -24,7 +24,7 @@ func TestHTMLExporter(t *testing.T) {
 	expectedWithoutHeader := "\n<!DOCTYPE html>\n<html>\n<head>\n    <meta charset='utf-8'>\n    <meta http-equiv='X-UA-Compatible' content='IE=edge'>\n    <title>Html Report</title>\n    <meta name='viewport' content='width=device-width, initial-scale=1'>\n</head>\n<body>\n\t<table>\n\t<tr>\n\t\t\n\t</tr>\n\t<tbody>\n\t\t\n\t\t\t<tr>\n\t\t\t\t\n\t\t\t\t\t<td>/tmp/test.png</td>\n\t\t\t\t\n\t\t\t\t\t<td>77.12</td>\n\t\t\t\t\n\t\t\t\t\t<td>12.56</td>\n\t\t\t\t\n\t\t\t</tr>\n\t\t\n\t\t\t<tr>\n\t\t\t\t\n\t\t\t\t\t<td>/tmp/test2.png</td>\n\t\t\t\t\n\t\t\t\t\t<td>77.13</td>\n\t\t\t\t\n\t\t\t\t\t<td>12.52</td>\n\t\t\t\t\n\t\t\t</tr>\n\t\t\n\t\t\t<tr>\n\t\t\t\t\n\t\t\t\t\t<td>/tmp/test3.png</td>\n\t\t\t\t\n\t\t\t\t\t<td>77.10</td>\n\t\t\t\t\n\t\t\t\t\t<td>12.51</td>\n\t\t\t\t\n\t\t\t</tr>\n\t\t\n\t</tbody>\n\t</table>\n</body>\n</html>\n"
 	expectedWithHeader := "\n<!DOCTYPE html>\n<html>\n<head>\n    <meta charset='utf-8'>\n    <meta http-equiv='X-UA-Compatible' content='IE=edge'>\n    <title>Html Report</title>\n    <meta name='viewport' content='width=device-width, initial-scale=1'>\n</head>\n<body>\n\t<table>\n\t<tr>\n\t\t\n\t\t\t<th>File</th>\n\t\t\n\t\t\t<th>Latitude</th>\n\t\t\n\t\t\t<th>Longitude</th>\n\t\t\n\t</tr>\n\t<tbody>\n\t\t\n\t\t\t<tr>\n\t\t\t\t\n\t\t\t\t\t<td>/tmp/test.png</td>\n\t\t\t\t\n\t\t\t\t\t<td>77.12</td>\n\t\t\t\t\n\t\t\t\t\t<td>12.56</td>\n\t\t\t\t\n\t\t\t</tr>\n\t\t\n\t\t\t<tr>\n\t\t\t\t\n\t\t\t\t\t<td>/tmp/test2.png</td>\n\t\t\t\t\n\t\t\t\t\t<td>77.13</td>\n\t\t\t\t\n\t\t\t\t\t<td>12.52</td>\n\t\t\t\t\n\t\t\t</tr>\n\t\t\n\t\t\t<tr>\n\t\t\t\t\n\t\t\t\t\t<td>/tmp/test3.png</td>\n\t\t\t\t\n\t\t\t\t\t<td>77.10</td>\n\t\t\t\t\n\t\t\t\t\t<td>12.51</td>\n\t\t\t\t\n\t\t\t</tr>\n\t\t\n\t</tbody>\n\t</table>\n</body>\n</html>\n"
 
-	t.Run("test csv export for simple document without header", func(t *testing.T) {
+	t.Run("test html export for simple document without header", func(t *testing.T) {
 
 		csvData, err := exporter.ExportAsBytes([]string{}, data)
 
@@ -33,7 +33,7 @@ func TestHTMLExporter(t *testing.T) {
 
 	})
 
-	t.Run("test csv export for simple document with header", func(t *testing.T) {
+	t.Run("test html export for simple document with header", func(t *testing.T) {
 
 		csvData, err := exporter.ExportAsBytes(header, data)
 
@@ -42,7 +42,7 @@ func TestHTMLExporter(t *testing.T) {
 
 	})
 
-	t.Run("test csv export for simple document as stream", func(t *testing.T) {
+	t.Run("test html export for simple document as stream", func(t *testing.T) {
 
 		buffer := new(bytes.Buffer)
 
